2afcc1a0fd3ba3f0469b690cffa200bb42325e73
Russell T. Griffith <russell.griffith@gmail.com>
2017-06-12 12:11:30 -0400

    Changes from Step 8: https://dev.office.com/sharepoint/docs/sp-add-ins/programmatically-deploy-a-custom-button-in-the-provider-hosted-add-in
    NOTES TAKEN FROM SharePointComponentDeployer.cs
    
    // Delete the original custom action.
                    webScopedEmployeeAction.DeleteObject();
                    // NOTE: Originally this action failed due to permissions.
                    // Upon research of this issue, I found this Stack Overflow Article.
                    // https://stackoverflow.com/questions/29675567/access-denied-office-365-sharepoint-online-with-global-admin-account
                    // Based on the provided solution, I wrote a quick app to check those permissions
                    // and indeed they were set to false as indicated. I reset them in the SP Admin
                    // however, it takes 24 hours for that setting to initiate. Once 24 hours had passed
                    // I retried again and it was successful.
                    /*
                     * This of course poses an undesirable solution due to having to reconfigure SP Admin to make something like this work.
                     * TODO: If this is found to be an issue in the future, This provided solution needs to be tried!
                     *  I will need to figure out how this actually works to get it to do this properly.
                     *  For the meantime, I will leave the SP Admin setting alone because it is only a Developer Account environment
                     *  but this solution needs to be looked into if this is ever needed in the future.
    Since any change to the scripting setting made through the SharePoint Online admin center may take up to 24 hours to take effect, you could enable scripting on a particular site collection immediately via CSOM API (SharePoint Online Client Components SDK) as demonstrated below:
    
    public static void DisableDenyAddAndCustomizePages(ClientContext ctx, string siteUrl)
    {
        var tenant = new Tenant(ctx);
        var siteProperties = tenant.GetSitePropertiesByUrl(siteUrl, true);
        ctx.Load(siteProperties);
        ctx.ExecuteQuery();
    
        siteProperties.DenyAddAndCustomizePages = DenyAddAndCustomizePagesStatus.Disabled;
        var result = siteProperties.Update();
        ctx.Load(result);
        ctx.ExecuteQuery();
        while (!result.IsComplete)
        {
            Thread.Sleep(result.PollingInterval);
            ctx.Load(result);
            ctx.ExecuteQuery();
        }
    }
    Usage
    
    using (var ctx = GetContext(webUri, userName, password))
    {
        using (var tenantAdminCtx = GetContext(tenantAdminUri, userName, password))
        {
             DisableDenyAddAndCustomizePages(tenantAdminCtx,webUri.ToString());
        }
        RegisterJQueryLibrary(ctx);
     }
    where
    
    public static void RegisterJQueryLibrary(ClientContext context)
    {
        var actions = context.Site.UserCustomActions;
        var action = actions.Add();
        action.Location = "ScriptLink";
        action.ScriptSrc = "~SiteCollection/Style Library/Scripts/jQuery/jquery.min.js";
        action.Sequence = 1482;
        action.Update();
        context.ExecuteQuery();
    }
                     * END OF TODO
                     */

--- 1_BeforeSharePointUI/ChainStoreWeb/Utilities/SharePointComponentDeployer.cs
+++ 1_BeforeSharePointUI/ChainStoreWeb/Utilities/SharePointComponentDeployer.cs
@@ -118,10 +118,107 @@ namespace ChainStoreWeb.Utilities
           
         }
 
+        private static void ChangeCustomActionRegistration()
+        {
+            using (var clientContext = sPContext.CreateUserClientContextForSPHost())
+            {
+                var query = clientContext.Web.UserCustomActions.Where(x => x.Name == "dc747915-c02a-4b66-a1c7-ead30fca94c6.AddEmployeeToCorpDB");
+                IEnumerable<UserCustomAction> matchingActions = clientContext.LoadQuery(query);
+                clientContext.ExecuteQuery();
+
+                UserCustomAction webScopedEmployeeAction = matchingActions.Single();
+
+                // Get a reference to the(empty) collection of custom actions
+                // that are registered with the custom list.
+                var queryForList = clientContext.Web.Lists.Where(x => x.Title == "Local Employees");
+                IEnumerable<List> matchingLists = clientContext.LoadQuery(queryForList);
+                clientContext.ExecuteQuery();
+
+                List employeeList = matchingLists.First();
+                var listActions = employeeList.UserCustomActions;
+                clientContext.Load(listActions);
+                listActions.Clear();
+
+                // Add a blank custom action to the list's collection.
+                var listScopedEmployeeAction = listActions.Add();
+
+                // Copy property values from the descriptively deployed
+                // custom action to the new custom action
+                listScopedEmployeeAction.Title = webScopedEmployeeAction.Title;
+                listScopedEmployeeAction.Location = webScopedEmployeeAction.Location;
+                listScopedEmployeeAction.Sequence = webScopedEmployeeAction.Sequence;
+                listScopedEmployeeAction.CommandUIExtension = webScopedEmployeeAction.CommandUIExtension;
+                listScopedEmployeeAction.Update();
+
+                // Delete the original custom action.         
+                webScopedEmployeeAction.DeleteObject();
+                // NOTE: Originally this action failed due to permissions.
+                // Upon research of this issue, I found this Stack Overflow Article.
+                // https://stackoverflow.com/questions/29675567/access-denied-office-365-sharepoint-online-with-global-admin-account
+                // Based on the provided solution, I wrote a quick app to check those permissions
+                // and indeed they were set to false as indicated. I reset them in the SP Admin
+                // however, it takes 24 hours for that setting to initiate. Once 24 hours had passed 
+                // I retried again and it was successful.
+                /*
+                 * This of course poses an undesirable solution due to having to reconfigure SP Admin to make something like this work.
+                 * TODO: If this is found to be an issue in the future, This provided solution needs to be tried!
+                 *  I will need to figure out how this actually works to get it to do this properly.
+                 *  For the meantime, I will leave the SP Admin setting alone because it is only a Developer Account environment 
+                 *  but this solution needs to be looked into if this is ever needed in the future.
+Since any change to the scripting setting made through the SharePoint Online admin center may take up to 24 hours to take effect, you could enable scripting on a particular site collection immediately via CSOM API (SharePoint Online Client Components SDK) as demonstrated below:
+
+public static void DisableDenyAddAndCustomizePages(ClientContext ctx, string siteUrl)
+{
+    var tenant = new Tenant(ctx);
+    var siteProperties = tenant.GetSitePropertiesByUrl(siteUrl, true);
+    ctx.Load(siteProperties);
+    ctx.ExecuteQuery();
+
+    siteProperties.DenyAddAndCustomizePages = DenyAddAndCustomizePagesStatus.Disabled;
+    var result = siteProperties.Update();
+    ctx.Load(result);
+    ctx.ExecuteQuery();
+    while (!result.IsComplete)
+    {
+        Thread.Sleep(result.PollingInterval);
+        ctx.Load(result);
+        ctx.ExecuteQuery();
+    }
+}
+Usage
+
+using (var ctx = GetContext(webUri, userName, password))
+{
+    using (var tenantAdminCtx = GetContext(tenantAdminUri, userName, password))
+    {                  
+         DisableDenyAddAndCustomizePages(tenantAdminCtx,webUri.ToString());
+    }
+    RegisterJQueryLibrary(ctx);
+ }
+where
+
+public static void RegisterJQueryLibrary(ClientContext context)
+{
+    var actions = context.Site.UserCustomActions;
+    var action = actions.Add();
+    action.Location = "ScriptLink";
+    action.ScriptSrc = "~SiteCollection/Style Library/Scripts/jQuery/jquery.min.js";
+    action.Sequence = 1482;
+    action.Update();
+    context.ExecuteQuery();
+}
+                 * END OF TODO
+                 */
+
+                clientContext.ExecuteQuery();
+
+            }
+        }
         internal static void DeployChainStoreComponentsToHostWeb(HttpRequest request)
         {
             // Deployment code goes here.
             CreateLocalEmployeesList();
+            ChangeCustomActionRegistration();
             RemoteTenantVersion = localVersion;
         }
     }
